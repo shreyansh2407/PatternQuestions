5592b2109746da3a18f5ec2db8e78ff72cfb7b07
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-05-22 00:11:45 +0530

    Pattern 10

---
+++ 10pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printPattern10(int n){
+    for(int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i>n) stars = 2*n-i;
+        // if(i>n)  stars = 2*n-i-1;
+        for(int j=1;j<=stars;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    printPattern10(5);
+    return 0;
+}
