5fb58fe3564b52b39ae995deb984551cb64c8a76
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-05-23 00:03:04 +0530

    Pattern 11

---
+++ 11pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void printPattern11(int n){
+    int start=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0) start=1;
+            else start=0;
+            for(int j=0;j<=i;j++){
+                cout<<start;
+                start=1-start;
+            }
+            cout<<endl;
+        }
+}
+int main(){
+    int n=4;
+    printPattern11(n);
+    return 0;
+}
