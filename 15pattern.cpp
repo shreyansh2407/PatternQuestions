105f721980a56226576d16ee02de7b49c744ceae
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-05-26 00:09:05 +0530

    Pattern 15

---
+++ 15pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void printPattern13(int n){
+    
+    for(int i=1;i<=n;i++){
+        int count=65;
+        for(int j=1;j<=n-i+1;j++){
+            cout<<char(count);
+            count++;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    printPattern13(4);
+    return 0;
+}
