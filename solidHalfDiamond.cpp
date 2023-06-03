ca40bb9639b9e446b0fccb15fd034ff8cc9f1a1d
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-06-03 10:44:50 +0530

    Solid half diamond

---
+++ solidHalfDiamond.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void solidHalfDiamond(int n){
+    for(int i=1;i<=2*n-1;i++){
+        int stars = i;
+        if(i>n)
+        stars = 2*n-i;
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+int main(){
+    solidHalfDiamond(5);
+    return 0;
+}
